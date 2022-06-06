33314371a961490513ad9a2f09e6229c4d4e731a
Gabriel Barros Feitosa Sá <54006467+Colgate13@users.noreply.github.com>
2022-06-06 20:07:19 -0300

    Create SimplesArray.mjs

---
+++ Exemple/Simple/SimplesArray.mjs
@@ -0,0 +1,40 @@
+import typesgine from 'typesgine-ascii';
+
+console.clear();
+
+const render = () => {
+  let final = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ];
+
+  for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < 24; j++) {
+      final[i][j] = Math.round(Math.random() * (0 - 9) + 9);
+    }
+    final[i][final[i].length - 1] = '\n';
+  }
+
+  return final;
+}
+
+const frameHandler = typesgine.FrameHandler.Handler((engineIo) => {
+  engineIo.render(render(), 24, 16);
+})
+
+const inputHandler = typesgine.InputHandler.Handler((engineIo, keyPress) => {
+  console.log("KeyPress: ", keyPress)
+})
+
+new typesgine.EngineIo({
+  fps: 60,
+  renderHandler: new typesgine.Render(new typesgine.RenderTerminal()),
+  frameHandler: frameHandler,
+  keypressHandler: inputHandler,
+});
